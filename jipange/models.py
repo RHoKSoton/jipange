0f4240f1ea884307e2dd1f58d69f4af87175cc7c
Dan Palmer <dan.palmer@me.com>
2012-12-01 17:02:42 +0000

    use our own PhoneUser model instead

--- jipange/models.py
+++ jipange/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django_twilio.models import Caller
-
-
-class Message(models.Model):
-    received = models.DateTimeField()
-    content = models.TextField()
-    sender = models.ForeignKey(Caller)
 
 
 class PhoneUser(models.Model):
     number = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=30)
+
+
+class Message(models.Model):
+    received = models.DateTimeField()
+    content = models.TextField()
+    sender = models.ForeignKey(PhoneUser)

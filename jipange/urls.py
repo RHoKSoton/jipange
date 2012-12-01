ad14c9e3a2fcfdcdc08ab0e78188cbb3f6b3a8e7
Dan Palmer <dan.palmer@me.com>
2012-12-01 14:33:35 +0000

    we need this in both debug and production mode

--- jipange/urls.py
+++ jipange/urls.py
@@ -15,11 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT
-        }),
-    )
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
+)

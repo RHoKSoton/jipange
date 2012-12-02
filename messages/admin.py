290cb6fe15b4d7fe6dc267e6e9e8459a84f8473d
Dan Palmer <dan.palmer@me.com>
2012-12-02 12:49:03 +0000

    Sending SMS

--- messages/admin.py
+++ messages/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 import models
 
 
-admin.site.register(models.Message)
+admin.site.register(models.IncomingMessage)
 admin.site.register(models.PhoneUser)
+admin.site.register(models.OutgoingMessage)
+admin.site.register(models.MessagingList)
